bf46e6dcf4e83c0bcc53f4d4cdd16cefc06bccc9
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-07-13 09:51:47 -0400

    added more tests for steps_between

--- tests/derive_step.rs
+++ tests/derive_step.rs
@@ -10,10 +10,40 @@ pub enum MyEnum {
 #[allow(dead_code)]
 const MY_ENUM_RANGE: std::ops::RangeInclusive<MyEnum> = MyEnum::A..=MyEnum::C;
 
-#[test]
-fn steps_between() {
-    use std::iter::Step;
-    assert_eq!(Step::steps_between(&MyEnum::A, &MyEnum::C), Some(2))
+macro_rules! test_steps_between {
+    {$enum:ident=> $($first:ident, $second:ident => $expected:expr),+} => {
+        paste::paste!{
+        mod steps_between {
+            use std::iter::Step;
+            use super::MyEnum;
+
+        $(
+            #[test]
+            #[allow(non_snake_case)]
+            fn [< $first _ $second >]() {
+                assert_eq!(
+                    Step::steps_between(&$enum::$first, &$enum::$second),
+                    $expected
+                )
+            }
+        )+
+        }
+        }
+    };
+}
+
+test_steps_between! { MyEnum=>
+    A, A => Some(0),
+    A, B => Some(1),
+    A, C => Some(2),
+
+    B, A => None,
+    B, B => Some(0),
+    B, C => Some(1),
+
+    C, A => None,
+    C, B => None,
+    C, C => Some(0)
 }
 
 #[test]
